53d7860c02251d18f29ff47eada79758009f6ffa
UlisesAndrade <ulises.andrade@uabc.edu.mx>
2022-03-01 21:15:44 -0800

    Problema2

--- ProyectoVacio/Assets/Scripts/Problema_2.cs
+++ ProyectoVacio/Assets/Scripts/Problema_2.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class Problema_2 : MonoBehaviour
 {
+    class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] numT = {-1,-1};
+        
+        for(int i=0;i<nums.length-1; i++)
+        {
+            boolean flag = false;
+            for(int j=i+1; j<nums.length; j++){
+                if((nums[i] + nums[j]) == target)
+                {
+                    numT[0] = i;
+                    numT[1] = j;
+                    flag = true;
+                    break;
+                } 
+                if(flag)
+                    break;
+            
+            }
+        }
+        return numT;
+    }
+}
     // Start is called before the first frame update
     void Start()
     {
