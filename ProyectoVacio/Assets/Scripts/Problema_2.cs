fdfead37da497bcaf2c28e12ae2bb155eedad9f8
UlisesAndrade <ulises.andrade@uabc.edu.mx>
2022-03-02 12:52:38 -0800

    Problema_2_Terminado

--- ProyectoVacio/Assets/Scripts/Problema_2.cs
+++ ProyectoVacio/Assets/Scripts/Problema_2.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Problema_2 : MonoBehaviour
 {
     public int[] myArray = new int[] {1,3,4,2,7,0};
-    int target=10;
-    for (int i = 0; i < myArray.Length; i += 1) {
-        for (int j = 0; j < myArray.Length; j += 1) {
-            // evitamos la comparación de un elemento consigo mismo
-            if (i != j && (myArray[i] + myArray[j]) == target) {
-                Debug.Log("Los numeros son: "+ j,i);
+    public int target=10;
+    void Update() 
+    {
+        for (int i = 0; i < myArray.Length; i += 1) 
+        {
+            for (int j = 0; j < myArray.Length; j += 1) 
+            {
+                // evitamos la comparación de un elemento consigo mismo
+                if (i != j && (myArray[i] + myArray[j]) == target) 
+                {
+                    Debug.Log("Los indices que resultan la suma correcta son: "+ j,i);
+                }
             }
         }
     }
